b11224428c144507348be5d262bb1e6fe0fde846
0-mayurkaretha <0.mayurkaretha@gmail.com>
2023-08-01 19:30:17 +0530

    complete app

---
+++ src/app/page.jsx
@@ -0,0 +1,46 @@
+"use client"
+
+import BlogCard from '../components/blogCard/BlogCard'
+import { blogs } from '../lib/data'
+import Image from 'next/image'
+import classes from './page.module.css'
+import { useState, useEffect} from "react"
+import NextCors from 'nextjs-cors';
+
+
+
+
+export default async function Home() {
+  // const blogs = await fetchBlogs()
+
+  const [blogs, setBlogs]=useState();
+
+  useEffect(()=>{
+    
+    async function fetchBlogs() {
+
+      const res = await fetch(
+        'http://localhost:3000/api/blog', {cache: 'no-store'}
+      );
+      const data = await res.json();
+      setBlogs(data);
+    }
+    fetchBlogs();
+  },[]);
+
+  if(!blogs){
+   return (<div>Loading...</div>)
+  }
+
+  return (
+   <div className={classes.container}>
+    {blogs?.length > 0 && <h2>WebDevMania&apos;s Blog Website</h2>}
+     <div className={classes.wrapper}>
+      {blogs?.length > 0 
+       ? blogs.map((blog) => (
+        <BlogCard key={blog._id} blog={blog}/>
+      )) : <h3 className={classes.noBlogs}>No blogs are currently in the</h3>}
+     </div>
+   </div>
+  )
+}
